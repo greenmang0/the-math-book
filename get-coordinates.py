c61a012cf4712acffe32d8b6241c58f0ea86c34c
Abhishek Amberkar <abhishek.amberkar@gmail.com>
2025-02-08 14:19:25 +0530

    Update the python script

--- get-coordinates.py
+++ get-coordinates.py
@@ -4,6 +4,7 @@ import math
 
 
 def get_coordinates(angle_deg, distance):
+    """Calculate co-ordinates given angle and distance."""
     angle_rad = math.radians(angle_deg)
     x = distance * math.cos(angle_rad)
     y = distance * math.sin(angle_rad)
@@ -11,6 +12,7 @@ def get_coordinates(angle_deg, distance):
 
 
 def get_coordinates2(angle_deg, x):
+    """Calculate y co-ordinates given angle and x co-ordinates."""
     angle_rad = math.radians(angle_deg)
     distance = x / math.cos(angle_rad)
     y = distance * math.sin(angle_rad)
@@ -18,8 +20,10 @@ def get_coordinates2(angle_deg, x):
 
 
 def calc_angle(angle1, angle2):
+    """Calculate the third angle."""
     return 180 - (angle1 + angle2)
 
 
-def calc_triangle_side(x, y):
+def calc_side(x, y):
+    """Calculate the third side of the triangle."""
     return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
